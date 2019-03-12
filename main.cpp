2a5bbfb6f43304f8baf1c7b537e06a73263d1a3b
Bob (KozlyukDA) <bob@example.com>
2019-03-12 16:23:39 +0300

    min

--- main.cpp
+++ main.cpp
@@ -4,18 +4,31 @@ using namespace std;
 
 int main()
 {
+    int a, b, max, min;
     cout << "Enter A  and B: ";
-    int a, b, max;
     cin >> a >> b;
+
+    if(a > b){
+        min = b;
+    }
+    else
+        min = a;
+
+
     if(a > b){
         max = a;
     }
     else
         max = b;
+
     cout << "A + B = " << a + b << '\n'
         << "A - B = " << a - b << '\n'
         << "A * B = " << a * b << '\n'
         << "A / B = " << a / b << '\n'
-        << "max = " << max << '\n';
+
+        << "max = " << max << '\n'
+
+        << "min = " << min << '\n';
+
     return 0;
 }
